b6b80f82cfe0d882f80fdf3db197270190769ac1
Kristian Bolino <kbolino@gmail.com>
2024-12-01 13:45:24 -0500

    Add type annotations, organize code

--- src/control.lua
+++ src/control.lua
@@ -1,5 +1,13 @@
--- memoize recipes with fluid products
+---@alias _AssemblingMachine {e: LuaEntity, r: LuaRecipe, q: LuaQualityPrototype, c: number, b: number}
+---@alias _EntityEvent {name: string, entity: LuaEntity}
 
+-----------------------------------------
+-- Memoize recipes with fluid products --
+-----------------------------------------
+
+---Returns the fluid products, if any, produced by the recipe.
+---@param recipe LuaRecipePrototype
+---@return FluidProduct[]
 function fluid_products_for_recipe(recipe)
   local fluid_products = {}
   for _, product in ipairs(recipe.products) do
@@ -10,6 +18,8 @@ function fluid_products_for_recipe(recipe)
   return fluid_products
 end
 
+---Map of names of recipes with fluid products to their fluid products.
+---@type table<string, FluidProduct[]>
 local recipes_with_fluid_products = {}
 
 for recipe_name, recipe in pairs(prototypes.recipe) do
@@ -19,8 +29,67 @@ for recipe_name, recipe in pairs(prototypes.recipe) do
   end
 end
 
--- set up and register event handlers
+-----------------------------------
+-- Track all assembling machines --
+-----------------------------------
 
+---Updates the tracked parameters of an assembling machine.
+---@param entity LuaEntity
+function update_assembling_machine(entity)
+  local recipe, quality = entity.get_recipe()
+  storage.assembling_machines[entity.unit_number] = {
+    e = entity,
+    r = recipe,
+    q = quality,
+    c = entity.crafting_progress,
+    b = entity.bonus_progress
+  }
+end
+
+---Called on any event that creates an assembling machine.
+---@param event _EntityEvent
+function on_built_event(event)
+  update_assembling_machine(event.entity)
+end
+
+---Called on any event that destroys an assembling machine.
+---@param event _EntityEvent
+function on_mined_event(event)
+  storage.assembling_machines[event.entity.unit_number] = nil
+end
+
+local event_filters = { { filter = "type", type = "assembling-machine" } }
+script.on_event(defines.events.on_built_entity, on_built_event, event_filters)
+script.on_event(defines.events.on_robot_built_entity, on_built_event, event_filters)
+script.on_event(defines.events.on_space_platform_built_entity, on_built_event, event_filters)
+script.on_event(defines.events.on_entity_died, on_mined_event, event_filters)
+script.on_event(defines.events.on_player_mined_entity, on_mined_event, event_filters)
+script.on_event(defines.events.on_robot_mined_entity, on_mined_event, event_filters)
+script.on_event(defines.events.on_space_platform_mined_entity, on_mined_event, event_filters)
+
+script.on_configuration_changed(
+  function(_)
+    log("on_configuration_changed")
+    ---@type table<number, _AssemblingMachine>
+    storage.assembling_machines = {}
+    for _, surface in pairs(game.surfaces) do
+      local entities = surface.find_entities_filtered { type = "assembling-machine" }
+      for _, entity in ipairs(entities) do
+        update_assembling_machine(entity)
+      end
+    end
+  end
+)
+
+--------------------------------------------------------------
+-- Produce bonus fluids on the tick that crafting completes --
+--------------------------------------------------------------
+
+---@param machine _AssemblingMachine
+---@param recipe LuaRecipe
+---@param quality LuaQualityPrototype
+---@param fluid_products FluidProduct[]
+---@param bonus_per_quality_level number
 function calculate_and_insert_bonus_fluids(machine, recipe, quality, fluid_products, bonus_per_quality_level)
   local bonuses = 0
   local entity = machine.e
@@ -35,7 +104,7 @@ function calculate_and_insert_bonus_fluids(machine, recipe, quality, fluid_produ
     if bonuses > 0 then
       for _, product in ipairs(fluid_products) do
         local amount_to_insert = bonus_per_quality_level * bonuses * product.amount
-        entity.insert_fluid{
+        entity.insert_fluid {
           name   = product.name,
           amount = amount_to_insert
         }
@@ -44,6 +113,8 @@ function calculate_and_insert_bonus_fluids(machine, recipe, quality, fluid_produ
   end
 end
 
+---@param machine _AssemblingMachine
+---@param bonus_per_quality_level number
 function on_tick_assembling_machine(machine, bonus_per_quality_level)
   local entity = machine.e
   local recipe, quality = entity.get_recipe()
@@ -57,53 +128,12 @@ function on_tick_assembling_machine(machine, bonus_per_quality_level)
   return nil
 end
 
-function update_assembling_machine(entity)
-  local recipe, quality = entity.get_recipe()
-  storage.assembling_machines[entity.unit_number] = {
-    e = entity,
-    r = recipe,
-    q = quality,
-    c = entity.crafting_progress,
-    b = entity.bonus_progress
-  }
-end
-
-function on_built_event(event)
-  update_assembling_machine(event.entity)
-end
-
-function on_mined_event(event)
-  storage.assembling_machines[event.entity.unit_number] = nil
-end
-
-local event_filters = {{filter = "type", type = "assembling-machine"}}
-script.on_event(defines.events.on_built_entity, on_built_event, event_filters)
-script.on_event(defines.events.on_robot_built_entity, on_built_event, event_filters)
-script.on_event(defines.events.on_space_platform_built_entity, on_built_event, event_filters)
-script.on_event(defines.events.on_entity_died, on_mined_event, event_filters)
-script.on_event(defines.events.on_player_mined_entity, on_mined_event, event_filters)
-script.on_event(defines.events.on_robot_mined_entity, on_mined_event, event_filters)
-script.on_event(defines.events.on_space_platform_mined_entity, on_mined_event, event_filters)
-
 script.on_event(defines.events.on_tick,
-  function(event)
+  function(_)
     bonus_per_quality_level = settings.global["fluid-quality-bonus-percent"].value / 100
     if storage.assembling_machines then
       for _, machine in pairs(storage.assembling_machines) do
         on_tick_assembling_machine(machine, bonus_per_quality_level)
-      end
-    end
-  end
-)
-
-script.on_configuration_changed(
-  function(data)
-    log("on_configuration_changed")
-    storage.assembling_machines = {}
-    for _, surface in pairs(game.surfaces) do
-      local entities = surface.find_entities_filtered{type = "assembling-machine"}
-      for _, entity in ipairs(entities) do
-        update_assembling_machine(entity)
       end
     end
   end
